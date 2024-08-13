f42d6540af94b93b3202d95bd5a6ce15216f0adf
saarizaws <saad8840@gmail.com>
2024-08-13 14:46:19 +0500

    third commit

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -28,8 +28,8 @@ const Approach = () => {
         <Card
           title="Development & Progress Update"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
+          des="Once we agree on the plan, We cue our lofi playlist and dive into
+          coding. From initial sketches to polished code, We keep you updated
           every step of the way."
         >
           <CanvasRevealEffect
@@ -51,7 +51,7 @@ const Approach = () => {
           title="Development & Launch"
           icon={<AceternityIcon order="Phase 3" />}
           des="This is where the magic happens! Based on the approved design, 
-          I'll translate everything into functional code, building your website
+          We'll translate everything into functional code, building your website
           from the ground up."
         >
           <CanvasRevealEffect
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -51,8 +51,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", ".NET", "Entity Framework"];
+  const rightLists = ["SQL Server", "Azure", "Stripe"];
 
   const [copied, setCopied] = useState(false);
 
